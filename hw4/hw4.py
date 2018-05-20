8c02d29f89d41fe3ced085fb1d9058db9c34cc55
maxwellre <yitianshao@gmail.com>
2018-05-19 20:12:19 -0700

    Completed Program 1.

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -36,6 +36,18 @@ def evalBCResult(pLabel, tLabel):
     print("Class 0 - error = {0:4.1f}%\nClass 1 - error = {1:4.1f}%\n".format(100*incorrPr0,100*incorrPr1))
     return incorrInd
 
+# Visualize data
+def disp2DResult(data_in, one_hot, disp_now=1, label_ext = ''):
+    label_num = one_hot.shape[1]
+    for i in range(label_num):
+        plt.scatter(data_in[one_hot[:,i],0],data_in[one_hot[:,i],1],s=5,label="Class {:2d} {:s}".format(i+1,label_ext))
+    plt.xlabel('Dimension 0',fontsize=10)
+    plt.ylabel('Dimension 1',fontsize=10)
+    plt_ax = plt.gca()
+    plt_ax.set_aspect('equal', 'box')
+    if disp_now:
+        plt.show()
+
 #-----------------------------------------------------------------------------------------------------------------------
 # Generating data
 data_num = 300
@@ -70,13 +82,13 @@ x1 = x1[np.random.permutation(data_num),:] # Reshuffle the gaussian mixture data
 
 print('C0 =\n',C0, '\n C1A =\n',C1A,'\n C1B =\n',C1B)
 
-trainEnd = int(data_num*0.6666)
+train_num = int(data_num*0.6666)
 
-xTrain = np.concatenate((x0[:trainEnd,:],x1[:trainEnd,:]),axis=0) # data
-tTrain = np.concatenate((np.zeros(trainEnd),np.ones(trainEnd))) # label
+xTrain = np.concatenate((x0[:train_num,:],x1[:train_num,:]),axis=0) # data
+tTrain = np.concatenate((np.zeros(train_num),np.ones(train_num))) # label
 
-xTest = np.concatenate((x0[trainEnd:,:],x1[trainEnd:,:]),axis=0) # data
-tTest = np.concatenate((np.zeros(data_num-trainEnd),np.ones(data_num-trainEnd))) # label
+xTest = np.concatenate((x0[train_num:,:],x1[train_num:,:]),axis=0) # data
+tTest = np.concatenate((np.zeros(data_num-train_num),np.ones(data_num-train_num))) # label
 
 # Shuffle the training set
 trainLen = xTrain.shape[0]
@@ -86,7 +98,46 @@ tTrain = tTrain[randInd]
 
 #-----------------------------------------------------------------------------------------------------------------------
 # 1) SVM-SMO
-clf = SVC()
-clf.fit(xTrain, tTrain)
-tPred = clf.predict(xTest)
-evalBCResult(tPred, tTest)
+plt.figure()
+plt.get_current_fig_manager().window.wm_geometry("1400x760+20+20")
+
+
+l = [0.1, 2.0, 10.0] # kernel radius
+
+for i in range(3):
+    gamma = 0.5/(l[i]**2)
+    clf = SVC(kernel='rbf', gamma=gamma)
+    clf.fit(xTrain, tTrain)
+    support_vect = clf.support_vectors_
+    vect_num = clf.n_support_
+    support_vect_ratio = 0.5*sum(vect_num)/train_num
+
+    tPred = clf.predict(xTest)
+    evalBCResult(tPred, tTest)
+
+    # Visualize data and classification error (and decision boundary)
+    plt.subplot2grid((1, 3), (0, i), rowspan=1, colspan=1)
+
+    plt.scatter(support_vect[:,0],support_vect[:,1],c='k',s=20,label="Support Vectors",facecolors=None)
+
+    disp2DResult(xTrain, np.column_stack((1 - tTrain, tTrain)).astype(bool),0,label_ext='Train')
+    disp2DResult(xTest, np.column_stack((1 - tTest, tTest)).astype(bool),0,label_ext='Test')
+
+    xRange = np.arange(min(np.min(xTrain[:,0]),np.min(xTest[:,0])),max(np.max(xTrain[:,0]),np.max(xTest[:,0])),0.05)
+    yRange = np.arange(min(np.min(xTrain[:,1]),np.min(xTest[:,1])),max(np.max(xTrain[:,1]),np.max(xTest[:,1])),0.05)
+    xGrid, yGrid = np.meshgrid(xRange, yRange, sparse=False, indexing='xy')
+    xGrid = np.reshape(xGrid, (xGrid.size,1))
+    yGrid = np.reshape(yGrid, (yGrid.size,1))
+    deciBoundX = np.column_stack((xGrid,yGrid))
+    classPr = clf.predict(deciBoundX)
+    plt.scatter(deciBoundX[(classPr == 1),0],deciBoundX[(classPr == 1),1],s=0.01,c='g',label='Class 1 Boundary')
+    if (i ==1):
+        (plt.gca()).legend(loc='upper left',bbox_to_anchor=(0.25, -0.15))
+    plt.title("l={0:.1f}, fraction of support vectors = {1:.1f}%" \
+     .format(l[i],100*support_vect_ratio),fontsize=12)
+
+plt.subplots_adjust(left=0.05, right=0.98, top=0.9, bottom=0.1)
+plt.show()
+
+#-----------------------------------------------------------------------------------------------------------------------
+# 2)
